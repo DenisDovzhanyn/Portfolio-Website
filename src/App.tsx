80fa1730cb786b08f12779411a30ddbe1e9fb339
Denis <dovzhanyndenis03@gmail.com>
2024-09-24 19:29:50 -0400

    added loading screen and updated repo photos for better aesthetic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './loading.css';
 import { getReposFromS3 } from './repo.service';
 import { Card } from './components/Card';
 import {Repository} from './types/repository'
 
 function App() {
   const [repos, setRepos] = useState<Repository[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRepos = async () => {
-      const repositoryInfo = await getReposFromS3();
-      setRepos(Object.values(repositoryInfo));
+      try {
+        const repositoryInfo = await getReposFromS3();
+        setRepos(Object.values(repositoryInfo));
+      } catch(error) {
+        console.log('error loading');
+      }
+      finally {
+        setLoading(false);
+      }
     }
     fetchRepos();
   }, []);
 
-  
+
+  if (loading) {
+    return (
+      <div id='loading'>Loading...</div>
+    )
+  }
+
   return (
     <div className="App">
       {repos.map((repo) => (
@@ -24,6 +39,7 @@ function App() {
       ))}
     </div>
   );
+
 }
 
 export default App;
