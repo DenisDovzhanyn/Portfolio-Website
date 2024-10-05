60ac4228c9383495ce6281992dc62537cd074a58
Denis <dovzhanyndenis03@gmail.com>
2024-10-05 19:12:19 -0400

    feat: finished footer. Now lets you email me and download resume link works. May switch text positions

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import {Repository} from './types/repository'
 import { ImageLink } from './components/ImageWithText';
 import gitHubIcon from './assets/Images/GitHub.svg';
 import LinkedinIcon from './assets/Images/Linkedin.svg';
+import EmailIcon from './assets/Images/Email.svg';
+import ResumeIcon from './assets/Images/Resume.svg';
 
 function App() {
   const [repos, setRepos] = useState<Repository[]>([]);
@@ -94,9 +96,9 @@ function App() {
   return (
     <div className="App" >
       <div className='head'>
-        <ImageLink text='Github' src={gitHubIcon} link='https://github.com/DenisDovzhanyn'/>
+        <ImageLink text='Github' src={gitHubIcon} link='https://github.com/DenisDovzhanyn' isDownload={false}/>
         <h1 id='name'>Denis Dovzhanyn</h1>
-        <ImageLink text='Linkedin' src= {LinkedinIcon} link='https://www.linkedin.com/in/denis-dovzhanyn/'/>
+        <ImageLink text='Linkedin' src= {LinkedinIcon} link='https://www.linkedin.com/in/denis-dovzhanyn/' isDownload={false}/>
       </div>
       
       <div className='main'>
@@ -139,6 +141,10 @@ function App() {
             <path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zm395.3 11.3l-112 112c-4.6 4.6-11.5 5.9-17.4 3.5s-9.9-8.3-9.9-14.8l0-64-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-64c0-6.5 3.9-12.3 9.9-14.8s12.9-1.1 17.4 3.5l112 112c6.2 6.2 6.2 16.4 0 22.6z"/>
           </svg>
         </motion.button>
+      </div>
+      <div className='footer'>
+        <ImageLink text='Contact me' src={EmailIcon} link='mailto:denisdovzhanyn03@gmail.com' isDownload={false}/>
+        <ImageLink text='Download my resume' src={ResumeIcon} link={require('./assets/Denis-Dovzhanyn-RESUME.pdf')} isDownload={true}/>
       </div>
     </div>
   );
--- src/components/ImageWithText/index.tsx
+++ src/components/ImageWithText/index.tsx
@@ -5,15 +5,16 @@ interface IProps {
     src: any
     text: string
     link: string
+    isDownload: boolean
 }
 
-export function ImageLink({src, text, link}: IProps) {
+export function ImageLink({src, text, link, isDownload}: IProps) {
     return (
         <div className='TextWithImage'>
-            <a className='imageLink' href={link}>
+            <a className='imageLink' download={isDownload && 'Denis-Dovzhanyn-Resume'} href={link}>
                 <img src={src} className='headerFooterImg'/>
             </a>
-            <a className='textLink' href={link}>
+            <a className='textLink' download={isDownload && 'Denis-Dovzhanyn-Resume'} href={link}>
                 <h2 className='headerFooterH2'>
                     {text}
                 </h2>
